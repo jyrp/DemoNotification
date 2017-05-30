fa7cef80c4057da78c8aa0ab179e26f6d1b38bac
15017135 <15017135@myrp.edu.sg>
2017-05-30 10:47:23 +0800

    sound

--- app/src/main/java/com/example/a15017135/demonotification/MainActivity.java
+++ app/src/main/java/com/example/a15017135/demonotification/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -42,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
                 builder.setSmallIcon(R.mipmap.ic_launcher);
                 builder.setContentIntent(pIntent);
                 builder.setAutoCancel(true);
+                Uri uri= RingtoneManager.getDefaultUri
+                        (RingtoneManager.TYPE_NOTIFICATION);
+                builder.setSound(uri);
+
 
                 Notification n = builder.build();
                 NotificationManager notificationManager = (NotificationManager)
